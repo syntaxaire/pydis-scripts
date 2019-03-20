b9fad83a6c83458208743a1dbb2044fdd52e95ff
syntaxaire <syntaxaire@gmail.com>
2019-03-20 18:10:19 -0400

    Add .flake8, change var names, Py2 compatibility

--- pydis-diagnose.py
+++ pydis-diagnose.py
@@ -1,6 +1,8 @@
 # Report information to help find common problems with missing PATH entries
 # or virtual environment/system interpreter mismatches.
-# From the Python Discord: https://pythondiscord.com/invite
+# From the Python Discord: https://pythondiscord.com/
+
+from __future__ import print_function
 
 import os
 import subprocess
@@ -12,27 +14,32 @@ verstr = str(ver[0]) + '.' + str(ver[1]) + '.' + str(ver[2])
 exe = sys.executable
 print('Script running in Python %s from %s' % (verstr, exe))
 
-# if we are in a virtual environment, this will not match
-print('Base prefix of the above executable is %s\n' % sys.base_prefix)
+# are we in a virtual environment?
+if ver[0] == 2:  # Python 2
+    if hasattr(sys, 'real_prefix'):
+        Print('Running in a virtual environment at %s' % sys.real_prefix)
+if ver[0] == 3:  # Python 3
+    print('Base prefix of the above executable is %s\n' % sys.base_prefix)
 
 # search the PATH environment variable for relevant executables
+# and store as lists of full paths to the targets
 targets = {'py.exe': [],
            'python.exe': [],
            'pip.exe': [],
            }
 paths = os.environ.get('PATH').split(';')
 for path in paths:
-    for target in targets:
-        tentative = os.path.join(path, target)
+    for exe in targets:
+        tentative = os.path.join(path, exe)
         if os.path.exists(tentative):
-            targets[target].append(tentative)
+            targets[exe].append(tentative)
 
-# print the full path to each found executable
-for target in targets:
-    if len(targets[target]) > 0:
-        print('Found %s on PATH:' % target)
-        for entry in targets[target]:
-            print('    %s' % entry)
-            runstr = '"%s" ' % entry + '--version'
-            complete = subprocess.run(runstr, capture_output=True, text=True)
-            print('    %s' % complete.stdout.split('\n')[0])
+# print the full path to each found executable, and call them to get version
+for exe in targets:
+    if len(targets[exe]) > 0:
+        print('Found %s on PATH:' % exe)
+        for full_path in targets[exe]:
+            print('    %s' % full_path)
+            run_str = '"%s" ' % full_path + '--version'
+            run_version = subprocess.run(run_str, capture_output=True, text=True)
+            print('    %s' % run_version.stdout.split('\n')[0])
